36d9bc78cfcdf15ff61028f5d23cd9d2a60a5b31
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-11-04 15:38:46 +0000

    Fragment duvida feito

--- app/src/main/java/com/fct/miei/ipm/fragments/Duvidas/CriarDuvidas.java
+++ app/src/main/java/com/fct/miei/ipm/fragments/Duvidas/CriarDuvidas.java
@@ -1,21 +1,22 @@
 package com.fct.miei.ipm.fragments.Duvidas;
 
-import android.content.Context;
-import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.os.Build;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.brutal.ninjas.hackaton19.R;
-import com.fct.miei.ipm.semNet;
 
-public class CriarDuvidas extends Fragment {
+public class CriarDuvidas extends Fragment implements AdapterView.OnItemSelectedListener {
+
+
+    private Spinner spinner;
+    private static final String[] paths = {"Público", "Amigos", "????"};
 
 
     public CriarDuvidas() {
@@ -23,22 +24,46 @@ public class CriarDuvidas extends Fragment {
     }
 
 
-    public boolean isInternetAvailable() {
-        ConnectivityManager cm = (ConnectivityManager) getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
-        return cm.getActiveNetworkInfo() != null;
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
-
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_ranking, container, false);
+        View view = inflater.inflate(R.layout.fragment_criar_duvida, container, false);
+
+        spinner = (Spinner) view.findViewById(R.id.spinner1);
+        ArrayAdapter<String>adapter = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
 
         return view;
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+
+        switch (position) {
+            case 0:
+                // Whatever you want to happen when the first item gets selected
+                break;
+            case 1:
+                // Whatever you want to happen when the second item gets selected
+                break;
+            case 2:
+                // Whatever you want to happen when the thrid item gets selected
+                break;
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 
 }
