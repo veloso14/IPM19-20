4ec01af6e0d1701c0b692647da7ba44760c20e40
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-11-05 14:40:20 +0000

    Layout apontamentos feito

---
+++ app/src/main/java/com/fct/miei/ipm/fragments/Duvidas/CriarApontamento.java
@@ -0,0 +1,41 @@
+package com.fct.miei.ipm.fragments.Duvidas;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.brutal.ninjas.hackaton19.R;
+import com.fct.miei.ipm.semNet;
+
+public class CriarApontamento extends Fragment {
+
+
+    public CriarApontamento() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_ranking, container, false);
+
+
+
+
+        return view;
+    }
+
+}
