221a9f11b64865c8c98c341e4bdc2931c52ac4b0
Camipique <c.quendera@campus.fct.unl.pt>
2019-11-08 19:31:19 +0000

    Redireciona para o criar apontamento e duvida

--- app/src/main/java/com/fct/miei/ipm/fragments/Adicionar.java
+++ app/src/main/java/com/fct/miei/ipm/fragments/Adicionar.java
@@ -1,14 +1,19 @@
 package com.fct.miei.ipm.fragments;
 
+import android.app.FragmentTransaction;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.brutal.ninjas.hackaton19.R;
+import com.fct.miei.ipm.fragments.Documentos.CriarApontamento;
+import com.fct.miei.ipm.fragments.Duvidas.Duvidas;
 
 public class Adicionar extends Fragment {
 
@@ -25,7 +30,34 @@ public class Adicionar extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_adicionar, container, false);
 
+
+        ImageView duvidas = view.findViewById(R.id.duvidas);
+
+        duvidas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.support.v4.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.content, new Duvidas());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+        //Adicionar um apontamento
+        ImageView apontamentos = view.findViewById(R.id.novoDocumento);
+
+        apontamentos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                android.support.v4.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.content, new CriarApontamento());
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
         return view;
     }
-
 }
