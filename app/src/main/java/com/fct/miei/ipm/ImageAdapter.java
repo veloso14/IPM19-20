0875cd888604bc3ae5afc0e667c523775439017e
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-11-08 02:46:03 +0000

    Geração das cadeiras dinamicamente

--- app/src/main/java/com/fct/miei/ipm/ImageAdapter.java
+++ app/src/main/java/com/fct/miei/ipm/ImageAdapter.java
@@ -2,23 +2,49 @@ package com.fct.miei.ipm;
 
 import android.app.FragmentTransaction;
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.brutal.ninjas.hackaton19.R;
 import com.fct.miei.ipm.fragments.Documentos.Documentos;
 
 public class ImageAdapter extends BaseAdapter {
     // Keep all Images in array
+    //Antigo
+    //TODO aproveitar os cantos ou ficam redondos??
     public Integer[] thumbnails = {
             R.drawable.aa, R.drawable.spbd,
             R.drawable.am, R.drawable.rit,
             R.drawable.pte, R.drawable.st,
-            R.drawable.trsa,
+
+    };
+
+
+    public String[] coresCadeiras = {
+            "#F99E2C",
+            "#23B04C",
+            "#B71414",
+            "#3A64B0" ,
+            "#D6D412",
+            "#843494"
+
+    };
+
+    public String[] nomesCadeiras = {
+            "AA",
+            "SPBD",
+            "AM",
+            "RIT" ,
+            "PTE",
+            "ST"
+
     };
     private Context mContext;
 
@@ -29,12 +55,12 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return thumbnails.length;
+        return nomesCadeiras.length;
     }
 
     @Override
     public Object getItem(int position) {
-        return thumbnails[position];
+        return nomesCadeiras[position];
     }
 
     @Override
@@ -44,9 +70,15 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView = new ImageView(mContext);
-        imageView.setImageResource(thumbnails[position]);
-        imageView.setOnClickListener((event) -> {
+
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.feed_item_layout, parent,
+                false);
+
+        ImageView background = rowView.findViewById(R.id.background);
+        background.setOnClickListener((event) -> {
             FragmentManager manager = ((AppCompatActivity) mContext).getSupportFragmentManager();
             android.support.v4.app.FragmentTransaction ft = manager.beginTransaction();
             ft.replace(R.id.content, new Documentos());
@@ -54,11 +86,16 @@ public class ImageAdapter extends BaseAdapter {
             ft.addToBackStack(null);
             ft.commit();
         });
-        imageView.setAdjustViewBounds(true);
+        background.setAdjustViewBounds(true);
         int padding = 8 * 4;
-        imageView.setPadding(padding, padding, padding, padding); // why not be explicit about the padding while we're at it
+        background.setPadding(padding, padding, padding, padding);
+        background.setBackgroundColor(Color.parseColor(coresCadeiras[position]));
+        //Titulo Cadeira
+        TextView cadeira  = rowView.findViewById(R.id.texto);
+        cadeira.setText(nomesCadeiras[position]);
 
-        return imageView;
+
+        return rowView;
     }
 
 }
