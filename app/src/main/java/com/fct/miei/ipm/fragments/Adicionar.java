aad29e41e841d18d5c48cc2f6e467d9e9a48b62b
Camipique <c.quendera@campus.fct.unl.pt>
2019-11-08 15:51:37 +0000

    added buttons

--- app/src/main/java/com/fct/miei/ipm/fragments/Adicionar.java
+++ app/src/main/java/com/fct/miei/ipm/fragments/Adicionar.java
@@ -1,10 +1,12 @@
 package com.fct.miei.ipm.fragments;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.brutal.ninjas.hackaton19.R;
 
@@ -22,6 +24,18 @@ public class Adicionar extends Fragment {
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_adicionar, container, false);
+
+        ImageButton addDocumentBtn = view.findViewById(R.id.addDocumentButton);
+
+        addDocumentBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ImageButton btn = (ImageButton) view;
+                        btn.setVisibility(View.GONE);
+                    }
+                }
+        );
 
         return view;
     }
