72fb86a3e00571fac15e4f5aef063d34ec2db5e1
João Veloso <jm.veloso@campus.fct.unl.pt>
2019-11-08 05:24:10 +0000

    Comentários Serializados

--- app/src/main/java/com/fct/miei/ipm/fragments/Comentarios/Comentarios.java
+++ app/src/main/java/com/fct/miei/ipm/fragments/Comentarios/Comentarios.java
@@ -1,11 +1,14 @@
 package com.fct.miei.ipm.fragments.Comentarios;
 
 import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -15,13 +18,16 @@ import com.brutal.ninjas.hackaton19.R;
 import com.fct.miei.ipm.fragments.Duvidas.Duvidas;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class Comentarios extends Fragment {
 
 
     private static CustomAdapter adapter;
-    ArrayList<ComentarioModel> dataModels;
-    ListView listView;
+    private ArrayList<ComentarioModel> dataModels;
+    private ListView listView;
 
     public Comentarios() {
         // Required empty public constructor
@@ -48,6 +54,34 @@ public class Comentarios extends Fragment {
                     Toast.makeText(getContext(),"Comentário não pode estar vazio" , Toast.LENGTH_SHORT).show();
                 }
                 else {
+                    //Adiciona
+                    SharedPreferences prefs = getActivity().getSharedPreferences("Comentarios", MODE_PRIVATE);
+                    String parse = prefs.getString("Comentarios", "");//The default value.
+                    String[] comentarios = parse.split(",");
+                    comentarios = increaseArray(comentarios, 1);
+                    comentarios[comentarios.length - 1] = comentario;
+
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < comentarios.length; i++) {
+                        sb.append(comentarios[i]).append(",");
+                    }
+
+                    SharedPreferences.Editor editor = getActivity().getSharedPreferences("Comentarios", MODE_PRIVATE).edit();
+                    editor.putString("Comentarios", sb.toString());
+                    editor.apply();
+
+                    dataModels.add(new ComentarioModel(R.drawable.perfil_joao, comentario));
+                    //Fecha o teclado
+                    InputMethodManager inputManager = (InputMethodManager) getActivity()
+                            .getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                            InputMethodManager.HIDE_NOT_ALWAYS);
+                    //Limpa o editext
+                    comment.getText().clear();
+                    //Refresh vier
+                    listView.invalidateViews();
+                    listView.setSelection(dataModels.size() - 1);
+
 
                 }
             }
@@ -70,20 +104,37 @@ public class Comentarios extends Fragment {
         //Comentários load
         listView = view.findViewById(R.id.list);
 
-        dataModels = new ArrayList<>();
+        SharedPreferences prefs = getActivity().getSharedPreferences("Comentarios", MODE_PRIVATE);
+        String parse = prefs.getString("Comentarios", "");//The default value.
+        String[]  comentariodUser = parse.split(",");
 
+        dataModels = new ArrayList<>();
         //Dummy data
         dataModels.add(new ComentarioModel(R.drawable.pessoa7, "Tenho isso resolvido no caderno mas não percebi"));
         dataModels.add(new ComentarioModel(R.drawable.pessoa6, "Same \uD83D\uDE2D\uD83D\uDE2D\uD83D\uDE2D"));
+        for(int i = 1 ; i <  comentariodUser.length ; i++){
+            dataModels.add(new ComentarioModel(R.drawable.perfil_joao, comentariodUser[i]));
+        }
         //Check for user coments
-
 
         adapter = new CustomAdapter(dataModels, getContext());
 
         listView.setAdapter(adapter);
 
+        listView.setSelection(dataModels.size() - 1);
 
         return view;
+    }
+
+
+    public String[] increaseArray(String[] theArray, int increaseBy) {
+        int i = theArray.length;
+        int n = ++i;
+        String[] newArray = new String[n];
+        for (int cnt = 0; cnt < theArray.length; cnt++) {
+            newArray[cnt] = theArray[cnt];
+        }
+        return newArray;
     }
 
 
